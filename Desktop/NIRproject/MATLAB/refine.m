9eeddb47ef3df4359df06844933a6f52f1123bae
w026ddd <dias.5@wright.edu>
2015-10-20 10:36:31 -0400

    Update refine.m

--- Desktop/NIRproject/MATLAB/refine.m
+++ Desktop/NIRproject/MATLAB/refine.m
@@ -1,13 +1,20 @@
+% This code averanges the signal py 
+
 function [px] = refine(py)
 
+% px resulting array is initialized 
 px = py;
 
+% the mean and standard deviation of the signal py are calculated 
 m = mean(py(1:end,2));
 s = std(py(1:end,2));
 
+% the py`s signal amplitude of signal is reduced by the technique resulting on px 
 for i = 1:length(py)
+   % if a point on th py is bigger than py`s mean the point is subtracted by the  py`s standard deviation
    if(py(i,2) > m)
    px(i,2) = py(i,2) - s;    
+   % if a point on th py is shorter than py`s mean the point is added by the py`s standard deviation
    else
    px(i,2) = py(i,2) + s;    
    end
